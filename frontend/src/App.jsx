ba0b06228e9c61fdd44a88c47dad03d81540978c
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-12 14:23:15 -0600

    Remove sampleDataForPhotoListItem from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,39 +4,6 @@ import React from 'react';
 import PhotoList from './components/PhotoList';
 import './App.scss';
 
-const sampleDataForPhotoListItem = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  }
-];
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
