e174caaf9cc3659ca372a85c860891fac8ebdf60
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-12 13:12:28 -0600

    Display threedifferent PhotoListItem components by using an Array helper method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,49 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const sampleDataForPhotoListItem = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  },
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  }
+];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  const photoItems = sampleDataForPhotoListItem.map((data) => (
+    <PhotoListItem key={data.id} data={data} />
+  ));
+
   return (
     <div className="App">
-      <PhotoListItem data={sampleDataForPhotoListItem} />
+      {photoItems}
     </div>
   );
 };
