5323032ba6f65d4ce0e1260fd774784123a7bf46
Shivangi Srivastava <shi06dec@gmail.com>
2023-09-14 18:41:36 -0600

    Create the Home Page Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import TopNavigationBar from './components/TopNavigationBar';
-import PhotoList from './components/PhotoList';
+import HomeRoute from './components/HomeRoute';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -9,8 +8,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
